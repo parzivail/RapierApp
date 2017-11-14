c661426919bcf2c2f6ee4ff1510528dfa9313839
parzivail <colby.newman2000@gmail.com>
2017-11-14 17:54:58 -0500

    fixed JS errors post-formatting

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function createWindow() {
 	win.webContents.openDevTools();
 
 	// Emitted when the window is closed.
-	win.on('closed', () = > {
+	win.on('closed', () => {
 		// Dereference the window object, usually you would store windows
 		// in an array if your app supports multi windows, this is the time
 		// when you should delete the corresponding element.
@@ -38,7 +38,7 @@ function createWindow() {
 app.on('ready', createWindow);
 
 // Quit when all windows are closed.
-app.on('window-all-closed', () = > {
+app.on('window-all-closed', () => {
 	// On macOS it is common for applications and their menu bar
 	// to stay active until the user quits explicitly with Cmd + Q
 	if (process.platform !== 'darwin'
@@ -49,7 +49,7 @@ app.on('window-all-closed', () = > {
 })
 ;
 
-app.on('activate', () = > {
+app.on('activate', () => {
 	// On macOS it's common to re-create a window in the app when the
 	// dock icon is clicked and there are no other windows open.
 	if (win === null
