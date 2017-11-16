4325c77b630019b5d521af4006ba4b37ae086a07
parzivail <colby.newman2000@gmail.com>
2017-11-16 09:29:46 -0500

    DLC manager tabs

--- web/js/app.js
+++ web/js/app.js
@@ -3,26 +3,23 @@
  */
 const nconf = require('nconf'),
 	fs = require('fs'),
-	rq = require('electron-require');
-rq.set('local', './web/js/modules');
+	rq = require('electron-require'),
+	temp = rq.set('local', './web/js/modules'),
+	fileutils = rq.local('./fileutils'),
+	by = rq.local('./by'),
+	alert = rq.local('./alert');
 
+/*
+ * Config
+ */
 nconf.file({file: 'config.json'});
 
 if (!nconf.get('quiplash'))
 	nconf.set('quiplash:path', "");
 
-const fileutils = rq.local('./fileutils'),
-	by = rq.local('./by'),
-	alert = rq.local('./alert');
-
-var app = new Vue({
-	el: "#rapier",
-	data: {
-		isQuiplashPathInvalid: false,
-		quipPath: nconf.get('quiplash:path')
-	}
-});
-
+/*
+ * UI Helpers
+ */
 function onQuiplashPathChange(newPath) {
 	app.quipPath = newPath || app.quipPath;
 	app.isQuiplashPathInvalid = !fileutils.isValidQuiplash(app.quipPath);
@@ -35,6 +32,35 @@ function onQuiplashPathChange(newPath) {
 	}
 }
 
+/*
+ * Vue App
+ */
+Vue.component('dlc-item', {
+	props: ['dlc'],
+	template: '<li><a href="#">{{ dlc.name }}</a></li>'
+});
+
+var app = new Vue({
+	el: "#rapier",
+	data: {
+		isQuiplashPathInvalid: false,
+		quipPath: nconf.get('quiplash:path'),
+		loadedDlc: [
+			{id: 0, name: "Quiplash Core", path: "/etc/quipdlc/core"},
+			{id: 999, name: "CoolDLC", path: "/etc/quipdlc/cooldlc"},
+			{id: 2938745, name: "BestDLC", path: "/etc/quipdlc/bestdlc"},
+		]
+	}
+});
+
+UIkit.tab("#tabDlc", {
+	connect: "#component-nav",
+	animation: "uk-animation-fade"
+});
+
+/*
+ * Events
+ */
 by.id("textQuiplash").onkeyup = function () {
 	app.isQuiplashPathInvalid = !fileutils.isValidQuiplash(app.quipPath);
 };
