14c21e1d792bc731f25020a314b7d03815cc721b
Colby Newman <colby.newman2000@gmail.com>
2017-11-20 21:30:52 -0500

    more complete pack editor, framework for saving packs

--- web/js/modules/quiplash-structs/questionpack.js
+++ web/js/modules/quiplash-structs/questionpack.js
@@ -18,7 +18,69 @@ QuestionPack.prototype.validate = function () {
 };
 
 QuestionPack.prototype.save = function () {
-	// TODO
+	var newManifest = {
+		id: "Pack1",
+		name: "DLC Pack 1",
+		types: ["Question"]
+	};
+
+	var newContent = {
+		episodeId: this.episodeId,
+		content: [
+			{
+				x: false,
+				id: 12345,
+				prompt: "Question thing"
+			}
+		]
+	};
+
+	var dataJet = {
+		fields: [{
+			t: "B",
+			v: true,
+			n: "HasJokeAudio"
+		}, {
+			t: "S",
+			v: "dinosaur|a dinosaur", // keywords.join('|')
+			n: "Keywords"
+		}, {
+			t: "S",
+			v: "",
+			n: "Author"
+		}, {
+			t: "S",
+			v: "A dinosaur? Get Jeff Goldblum to Scotland, STAT!",
+			n: "KeywordResponseText"
+		}, {
+			t: "S",
+			v: "What is the Loch Ness Monster, really?",
+			n: "PromptText"
+		}, {
+			t: "S",
+			v: "",
+			n: "Location"
+		}, {
+			t: "A",
+			v: "joke", // joke.mp3
+			n: "KeywordResponseAudio"
+		}, {
+			t: "A",
+			v: "vo", // vo.mp3
+			n: "PromptAudio"
+		}]
+	};
+
+	/*
+	 create folder {name = "Question"}
+	 foreach question
+	 if !exists (folder {name = question.id})
+	 create folder {name = question.id}
+	 populate folder
+	 vo.mp3
+	 joke.mp3
+	 write out data.jet
+	 */
 };
 
 QuestionPack.prototype.delete = function () {
